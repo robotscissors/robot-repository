6d4ffa503d9a56e76a1a1965b9efa5907ec1ffe9
robotscissors <cjbazin@gmail.com>
2017-08-08 22:37:06 -0700

    add admin page to edit and display users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Modified the original Devise to add in fullname. It requires this modification
   devise_for :users, :controllers => { registrations: 'registrations' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-
+  resources :user_admin
 
   get 'welcome/index'
 
