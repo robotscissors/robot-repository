429c2b337ec36a97eb28df0a57b3c540417a3cb8
robotscissors <cjbazin@gmail.com>
2017-08-19 22:19:09 -0700

    attach original to the text peice

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,18 +7,21 @@ class Document < ActiveRecord::Base
 
   #has_attached_file :document_file, :path => "/documents/:id/:style/:filename", :url => "/documents/preview/:filename", :styles => { :thumb => ["300x400>", :jpg] }, :processors => [:thumbnail]
   #has_attached_file :document_file, :path => "/documents/:id/:filename"
-  has_attached_file :document_file, :path => "/documents/:id/:filename",
+  has_attached_file :document_file, :path => "/documents/:id/:filename", :preserve_files => "true",
                     :styles => {
-                      :pdf => {
-                        :format => "pdf",
-                        :processors => [:docsplit_pdf]
-                      },
                       :text => {
                         :processors => [:docsplit_text],
                         :full_text_column => :file_full_text
-                      }
+                      },
+                      :pdf => {
+                       :format => "pdf",
+                       :processors => [:docsplit_pdf]
+                     }
+
+
                     }
 
+  #has_attached_file :document_file, :path => "/documents/:id/original/:filename"
 
 
   #validates_attachment :document_file, content_type: { content_type: ['application/msword', 'application/pdf','application/vnd.openxmlformats-officedocument.wordprocessingml.document'] }
