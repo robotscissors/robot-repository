cfa95d9f91f6084d2370a32bdd61bbfa4a9c489a
robotscissors <cjbazin@gmail.com>
2017-08-21 21:20:31 -0700

    testing and change edit_form to not include thumbnail or new upload

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,27 +4,32 @@ class Document < ActiveRecord::Base
   validates :user_id, presence: true
   validates :document_file, presence: true
   belongs_to :user
+  # validates_attachment :document_file, content_type: { content_type: ['application/msword', 'application/pdf','application/vnd.openxmlformats-officedocument.wordprocessingml.document'] }
 
-  #has_attached_file :document_file, :path => "/documents/:id/:style/:filename", :url => "/documents/preview/:filename", :styles => { :thumb => ["300x400>", :jpg] }, :processors => [:thumbnail]
-  #has_attached_file :document_file, :path => "/documents/:id/:filename"
   has_attached_file :document_file, :path => "/documents/:id/:filename", :preserve_files => "true",
                     :styles => {
                       :text => {
                         :processors => [:docsplit_text],
+                        :format => "txt",
                         :full_text_column => :file_full_text
-                      },
-                      :pdf => {
-                       :format => "pdf",
-                       :processors => [:docsplit_pdf]
-                     }
-
-
+                      }
                     }
 
+  # has_attached_file :document_file, :path => "/documents/:id/:filename", :preserve_files => "true",
+  #                   :styles => {
+  #                     :text => {
+  #                       :processors => [:docsplit_text],
+  #                       :format => "txt",
+  #                       :full_text_column => :file_full_text
+  #                     },
+  #                     :pdf => {
+  #                      :format => "pdf",
+  #                      :processors => [:docsplit_pdf]
+  #                    }
+  #                   }
+
+
   #has_attached_file :document_file, :path => "/documents/:id/original/:filename"
-
-
-  #validates_attachment :document_file, content_type: { content_type: ['application/msword', 'application/pdf','application/vnd.openxmlformats-officedocument.wordprocessingml.document'] }
   #validates_attachment_content_type :document_file, content_type: /\Aimage\/.*\z/
 
 
