c72cf042a94cb3a5be6eadff3b076856321bb1bc
robotscissors <cjbazin@gmail.com>
2017-08-29 22:22:34 -0700

    Get Actionmailer working with gmail

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -4,7 +4,8 @@ class Notification < ApplicationMailer
   def new_document(document)
     @greeting = "GENERAL NOTIFICATION"
     @document = document
-    @users_to_notify = User.pluck(:email).join(', ')
-    mail to: @users_to_notify , subject: "A new document has been added to the repository"
+    # @users_to_notify = User.pluck(:email).join(', ')
+    @users_to_notify = "cjbazin@gmail.com,bazin@usc.edu"
+    mail to: @users_to_notify , subject: "ROBOT SAYS: A new document has been added to the repository"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,27 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'robotscissors.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain'
+     }
+
+ # config.action_mailer.smtp_settings = {
+ #    :address   => "smtp.mandrillapp.com",
+ #    :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
+ #    :enable_starttls_auto => true, # detects and uses STARTTLS
+ #    :user_name => ENV["MANDRILL_USERNAME"],
+ #    :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+ #    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+ #    :domain => 'robotscissors.com', # your domain to identify your server when connecting
+ #  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
