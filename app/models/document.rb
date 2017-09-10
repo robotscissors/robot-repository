457cab85b4fd95d44bb3dd696641f62fef93d126
robotscissors <cjbazin@gmail.com>
2017-09-09 22:39:21 -0700

    configure mobile and clean code

--- app/models/document.rb
+++ app/models/document.rb
@@ -15,12 +15,4 @@ class Document < ActiveRecord::Base
                         :full_text_column => :file_full_text
                       }
                     }
-  # has_attached_file :document_file, :path => "/documents/:id/:filename", :preserve_files => "true",
-  #                   :styles => {
-  #                     :text => {
-  #                       :processors => [:docsplit_text],
-  #                       :format => "txt"
-  #                     }
-  #                   }
-
 end
